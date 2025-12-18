79d3fff9583b41f2527163c4c5d54cbb6d242a85
Tayl0rH <taylorher0601@gmail.com>
2025-12-18 16:15:26 +0100

    muy bien

---
+++ angular-events/src/app/events-show/events-show.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IEvent } from '../interfaces/i-event';
+
+@Component({
+  selector: 'app-events-show',
+  imports: [],
+  templateUrl: './events-show.html',
+  styleUrl: './events-show.css',
+})
+export class EventsShow {
+
+
+  events: IEvent[] = [
+    {
+      title: 'Evento 1',
+      image: 'img.png',
+      date: '2025-12-23',
+      description: 'Descripción 1',
+      price: 23,
+    },
+    {
+      title: 'Evento 2',
+      image: 'img2.png',
+      date: '2025-02-10',
+      description: 'Descripción 2',
+      price: 5.99,
+    }
+  ]
+}
