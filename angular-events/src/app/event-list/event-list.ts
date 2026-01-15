a050ef3049219a09419ffdb272b67502a3f5a09d
Tayl0rH <taylorher0601@gmail.com>
2026-01-15 17:07:10 +0100

    Mas angular

---
+++ angular-events/src/app/event-list/event-list.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { IEvent } from '../interfaces/i-event';
+import { NgClass } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+import { TitleFilterPipe } from '../pipes/title-filter-pipe';
+
+@Component({
+  selector: 'app-event-list',
+  imports: [NgClass, FormsModule, TitleFilterPipe],
+  templateUrl: './event-list.html',
+  styleUrl: './event-list.css',
+})
+
+// CLASS
+export class EventList {
+
+  filterSearch: string = ''
+
+  events: IEvent[] = [
+      {
+        id: 1,
+        title: 'Evento 1',
+        image: 'img.png',
+        date: '2025-12-23',
+        description: 'Descripción 1',
+        price: 23,
+      },
+      {
+        id: 2,
+        title: 'Evento 2',
+        image: 'img2.png',
+        date: '2025-02-10',
+        description: 'Descripción 2',
+        price: 5.99,
+      }
+    ]
+
+}
---
+++ angular-events/src/app/price-view/price-view.ts
@@ -0,0 +1,13 @@
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-view',
+  imports: [CurrencyPipe, DatePipe],
+  templateUrl: './price-view.html',
+  styleUrl: './price-view.css',
+})
+export class PriceView {
+  price = 49.99;
+  today =  new Date();
+}
